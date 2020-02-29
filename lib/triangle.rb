4ae7ec49a99705405e40105e2ae3d82489af01b2
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-29 04:35:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,5 +16,9 @@ class Triangle
       if (side1 == side2) && (side2 == side3)
         :equilateral
       elsif (side1 == side2) || (side2 == side3) || (side1 == side3)
-        :
+        :isoceles
+      elsif (side1 != side2) || (side2 != side3) || (side1 != side3)
+        :scalene
+    end 
+  end 
 end
