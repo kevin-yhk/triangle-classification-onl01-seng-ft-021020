1371da1640a272df4bb926085ba54b44737cbcfa
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-29 04:40:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   def kind
     if (side1 <= 0) || (side2 <= 0) || (side3 <= 0)
       raise TriangleError
-    elsif (side1 + side2 <= side3) || (side1 + side3 <= side2) || side2 + side3 <= side1)
+    elsif (side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1)
       raise TriangleError
     else 
       if (side1 == side2) && (side2 == side3)
@@ -22,7 +22,6 @@ class Triangle
       end 
     end
   end 
-  class TriangleError < StandardError
-  end 
+  
 end
 
